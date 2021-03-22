6645e13ffde64c21f6a3ec1dc31b8ccbab5c3c39
Manel Clos <manelclos@gmail.com>
2021-03-22 20:01:55 +0100

    fix deprecations

--- tests/urls.py
+++ tests/urls.py
@@ -1,6 +1,13 @@
-from django.conf.urls import url
+from django import VERSION
 from django.contrib import admin
 
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-]
+if VERSION >= (2, 0):
+    from django.urls import path
+    urlpatterns = [
+        path('admin/', admin.site.urls),
+    ]
+else:
+    from django.conf.urls import url
+    urlpatterns = [
+        url(r'^admin/', admin.site.urls),
+    ]
